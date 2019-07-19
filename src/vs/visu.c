f2aac17d9ed7db1fd52cd0c728fa57ceb2d39ae9
Harra Gysella goodbrother <hgysella@oa-a3.21-school.ru>
2019-07-19 20:10:22 +0300

    renew visu19

--- src/vs/visu.c
+++ src/vs/visu.c
@@ -6,7 +6,7 @@
 /*   By: hgysella <hgysella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 18:00:28 by hgysella          #+#    #+#             */
-/*   Updated: 2019/07/18 20:43:46 by hgysella         ###   ########.fr       */
+/*   Updated: 2019/07/19 20:04:34 by hgysella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <ncurses.h>
 
 #define COLOR_GRAY 8
+#define MAP_X 194
 
 void			print_map(t_cw *cw)
 {
@@ -22,89 +23,79 @@ void			print_map(t_cw *cw)
 	uint		m;
 	uint		i;
 	uint		j;
-	uint		x;
-	uint		y;
 
 	i = 0;
 	j = 0;
 	k = 1;
 	m = 1;
-	getmaxyx(cw->visu.map, y, x);
 	while (i < cw->num_of_champs && j < MEM_SIZE)
 	{
 		while (j < (MEM_SIZE / cw->num_of_champs) * (i + 1) && j < MEM_SIZE)
 		{
-			wattron(cw->visu.map, COLOR_PAIR(9) | A_BOLD);
+			wattron(cw->visu.map, COLOR_PAIR(9));
 			if (j < cw->champ[i].head.prog_size + (MEM_SIZE / cw->num_of_champs) * i)
 				wattron(cw->visu.map, COLOR_PAIR(i * 2 + 1));
-			mvwprintw(cw->visu.map, k, m++, "%.2x",cw->map[j++]);
+			mvwprintw(cw->visu.map, k, m++, "%.2x", cw->map[j++]);
 			m += 2;
-			if (m > x - 2 && k++)
+			if (m > MAP_X - 2 && k++)	
 				m = 1;
 		}
 		i++;
 	}
 }
 
-void			print_header(t_cw *cw)
+void			print_header_1(t_cw *cw)
 {
 	uint		k;
-	uint		m;
-	uint		i;
-	uint		j;
 
-	i = 0;
+	k = 10 + 4 * cw->num_of_champs;
 	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-	mvwprintw(cw->visu.header, 6, 1,"%s %d", "Cycle :", cw->cycles);
-	mvwprintw(cw->visu.header, 8, 1,"%s %d", "Processes :", cw->num_of_cars);
-	k = 10;
-	while (i < cw->num_of_champs)
-	{	
-		wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-		j = 0;
-		m = 13;
-		wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-		mvwprintw(cw->visu.header, k, 1, "%s%d : ",  "Player -", i + 1);
-		while (cw->champ[i].head.prog_name[j])
-		{
-			wattron(cw->visu.header, COLOR_PAIR(i * 2 + 1) | A_BOLD);
-			mvwprintw(cw->visu.header, k, m++, "%c", cw->champ[i].head.prog_name[j++]);
-			if (m >= cw->visu.col / 4 - 2 && k++)
-				m = 1;
-		}
-		wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-		mvwprintw(cw->visu.header, ++k, 5,"%-25s %d", "Last live :", cw->champ[i].last_live);
-		mvwprintw(cw->visu.header, ++k, 5,"%-25s %d", "Lives in current period :", cw->champ[i].lives);
-		k += 2;
-		i++;
-	}
-	
-	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-	mvwprintw(cw->visu.header, k++, 1,"%s", "Live breakdown for current period :");
+	mvwprintw(cw->visu.header, k, 1,"%s", "Live breakdown for current period :");
+	mvwprintw(cw->visu.header, k += 3, 1,"%s", "Live breakdown for last period :");
 	wattron(cw->visu.header, COLOR_PAIR(9) | A_BOLD);
-	mvwprintw(cw->visu.header, k++, 1,"%s", "[------------------------------------------------]");
-	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);	
-	mvwprintw(cw->visu.header, k++, 1,"%s", "Live breakdown for last period :");
-	wattron(cw->visu.header, COLOR_PAIR(9) | A_BOLD);
-	mvwprintw(cw->visu.header, k++, 1,"%s", "[------------------------------------------------]");
-	
+	mvwprintw(cw->visu.header, k -= 2, 1,"%s", "[------------------------------------------------]");
+	mvwprintw(cw->visu.header, k += 3, 1,"%s", "[------------------------------------------------]");
 	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-	mvwprintw(cw->visu.header, ++k, 1,"%s : %d", "CYCLE_TO_DIE", cw->cycle_to_die);
+	mvwprintw(cw->visu.header, k += 2, 1,"%s : %-10d", "CYCLE_TO_DIE", cw->cycle_to_die);
 	mvwprintw(cw->visu.header, k += 2, 1,"%s : %d", "CYCLE_DELTA", CYCLE_DELTA);
 	mvwprintw(cw->visu.header, k += 2, 1,"%s : %d", "NBR_LIVE", NBR_LIVE);
 	mvwprintw(cw->visu.header, k += 2, 1,"%s : %d", "MAX_CHECKS", MAX_CHECKS);
 }
 
+void			print_header(t_cw *cw)
+{
+	uint		k;
+	uint		i;
+
+	i = 0;
+	k = 9;
+	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
+	mvwprintw(cw->visu.header, 1, 1,"%s", "** PAUSED **");
+	mvwprintw(cw->visu.header, 3, 1,"%s %d", "Cycles/second limit :", 5);
+	mvwprintw(cw->visu.header, 6, 1,"%s %-6d", "Cycle :", cw->cycles);
+	mvwprintw(cw->visu.header, 8, 1,"%s %d", "Processes :", cw->num_of_cars);
+	while (i < cw->num_of_champs)
+	{	
+		mvwprintw(cw->visu.header, ++k, 1, "%s%d : ",  "Player -", i + 1);
+		wattron(cw->visu.header, COLOR_PAIR(i * 2 + 1) | A_BOLD);
+		mvwprintw(cw->visu.header, k++, 13, "%.52s", cw->champ[i].head.prog_name);	
+		wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
+		mvwprintw(cw->visu.header, k++, 5,"%-25s %d", "Last live :", cw->champ[i].last_live);
+		mvwprintw(cw->visu.header, k++, 5,"%-25s %d", "Lives in current period :", cw->champ[i++].lives);
+	}
+	print_header_1(cw);
+}
+
 void			print_menu(t_cw *cw)
 {
 	wattron(cw->visu.menu, COLOR_PAIR(10) | A_BOLD);
-	mvwprintw(cw->visu.menu, 1, cw->visu.col / 2 - 2, "%-20s",  "MENU");
+	mvwprintw(cw->visu.menu, 1, 129, "%s",  "MENU");
 	mvwprintw(cw->visu.menu, 2, 1, "%s",  "Q for exit");
 	mvwprintw(cw->visu.menu, 2, 20, "%s",  "Spase for pause / run");
 	mvwprintw(cw->visu.menu, 2, 50, "%s",  "Speed w / s");
-	wrefresh(cw->visu.menu);
 	keypad(cw->visu.menu, TRUE);
 	noecho();
+	wrefresh(cw->visu.menu);
 }
 
 void			visu_exit(t_cw *cw)
@@ -123,86 +114,65 @@ void			select_key(t_cw *cw, int key, int *w)
 		visu_exit(cw);
 	else if (key == 32)
 	{
-		mvwprintw(cw->visu.header, 1, 1,"%s", "** PAUSED **");
+		mvwprintw(cw->visu.header, 1, 1, "%s", "** PAUSED **");
 		wrefresh(cw->visu.header);
 		while ((key = wgetch(cw->visu.menu)) != 32)
 			if (key == 'q')
 				visu_exit(cw);
 		halfdelay(*w);	
-		mvwprintw(cw->visu.header, 1, 1,"%s", "** RUNNIG **");
+		mvwprintw(cw->visu.header, 1, 1, "%s", "** RUNNIG **");
 		wrefresh(cw->visu.header);	
 	}
 	else if (key == 'w')
 	{
 		halfdelay(++*w);
-		mvwprintw(cw->visu.header, 3, 1,"%s %5d", "Cycles/second limit :", *w);
+		mvwprintw(cw->visu.header, 3, 1, "%s %5d", "Cycles/second limit :", *w);
 		wrefresh(cw->visu.header);
 	}
 	else if (key == 's')
 	{
 		halfdelay(--*w);	
-		mvwprintw(cw->visu.header, 3, 1,"%s %5d", "Cycles/second limit :", *w);
+		mvwprintw(cw->visu.header, 3, 1, "%s %5d", "Cycles/second limit :", *w);
 		wrefresh(cw->visu.header);	
 	}	
-}
-
-void			vs_logi(t_cw *cw, uint8_t i_car, int *d)
-{
-	uint8_t		col;
-	int			x;
-	int			y;
-	int			j;
-
-	//col = -cw->car[IN(cw->car[i_car]->op_code)]->reg[0];
-	col = 1;
-	j = 0;
-	getmaxyx(cw->visu.map, y, x);
-	//mvwprintw(cw->visu.menu, 4, 25, "% col", col);
-	wattron(cw->visu.map, COLOR_PAIR(col * 2) | A_BOLD);
-	//mvwprintw(cw->visu.map, cw->car[i_car]->pc * 3 / x + 1, cw->car[i_car]->pc * 3 % x + 1, "%.2x", cw->map[cw->car[i_car]->pc]);
-	mvwprintw(cw->visu.map, i_car * 3 / (x - 2) + 1, i_car * 3 % (x - 2) + 1, "%.2x", cw->map[i_car]);
-	wrefresh(cw->visu.map);
-	//sleep(1);
-	mvwprintw(cw->visu.map, i_car * 3 / (x - 2) + 1, i_car * 3 % (x - 2) + 1, "%.2x", cw->map[i_car]);	
-	select_key(cw, wgetch(cw->visu.menu), d);
-	while (j < 4)
-	{
-		wattron(cw->visu.map, COLOR_PAIR(col * 2 + 1) | A_BOLD);	
-		mvwprintw(cw->visu.map, i_car * 3 / (x - 2) + 1, i_car * 3 % (x - 2) + 1, "%.2x", cw->map[i_car]);	
-		i_car++;	
-		j++;
-	}
-	wrefresh(cw->visu.map);
 }
 
 void			vs_backlight_car(t_cw *cw, size_t i_car, uint8_t mod)
 {
 	uint8_t		col;
-	int			x;
-	int			y;
 
 	col = -cw->car[i_car]->reg[0];
-	getmaxyx(cw->visu.map, y, x);
-	wattron(cw->visu.map, ((mod) ? COLOR_PAIR(col * 2) : COLOR_PAIR(col * 2 - 1)) | A_BOLD);
-	mvwprintw(cw->visu.map, cw->car[i_car]->pc * 3 / (x - 1) + 1, cw->car[i_car]->pc * 3 % (x - 1) + 1, "%.2x", cw->map[cw->car[i_car]->pc]);
+	wattron(cw->visu.map, (mod) ? COLOR_PAIR(col * 2) : COLOR_PAIR(col * 2 - 1));
+	mvwprintw(cw->visu.map, cw->car[i_car]->pc * 3 / (MAP_X - 2) + 1, cw->car[i_car]->pc * 3 % (MAP_X - 2) + 1, "%.2x", cw->map[cw->car[i_car]->pc]);
 	wrefresh(cw->visu.map);
 }
 
 void			vs_backlight_map(t_cw *cw, t_stack *st_op, uint8_t mod)
 {
-	int			x;
-	int			y;
-	uint8_t		j;
+	int			j;
 
 	j = 0;
-	getmaxyx(cw->visu.map, y, x);
 	wattron(cw->visu.map, COLOR_PAIR(st_op->id * 2 + 1) | (mod) ? A_BOLD : 0);
-	while (++j < st_op->size)
+	while (++j <= st_op->size)
 	{
-		mvwprintw(cw->visu.map, st_op->pc * 3 / (x - 1) + 1, st_op->pc * 3 % (x - 1) + 1, "%.2x", st_op->pc);
-		st_op->pc = PCV(st_op->pc + j);
+		mvwprintw(cw->visu.map, st_op->pc * 3 / (MAP_X - 2) + 1, st_op->pc * 3 % (MAP_X - 2) + 1, "%.2x", cw->map[st_op->pc]);
+		st_op->pc = PCV(++st_op->pc);
 	}
 	wrefresh(cw->visu.map);
+	wattroff(cw->visu.map, COLOR_PAIR(st_op->id * 2 + 1) | A_BOLD);	
+}
+
+void				print_cycle_to_die(t_cw *cw)
+{
+	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
+	mvwprintw(cw->visu.header, 16 + 4 * cw->num_of_champs, 16, "%-10d", cw->cycle_to_die);
+}
+
+void				print_cycles(t_cw *cw)
+{
+	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
+	mvwprintw(cw->visu.header, 6, 9, "%-6d", cw->cycles);
+	mvwprintw(cw->visu.header, 8, 13,"%-6d", cw->num_of_cars);
 }
 
 void			visu(t_cw *cw)
@@ -211,7 +181,7 @@ void			visu(t_cw *cw)
 
 	delay = 1;
 	select_key(cw, wgetch(cw->visu.menu), &delay);
-	print_header(cw);
+	print_cycles(cw);
 	wrefresh(cw->visu.header);
 	if (!cw->visu.st_op)
 		return ;
@@ -225,7 +195,7 @@ void			visu(t_cw *cw)
 		else
 			break ;
 	}
-		
+	
 }
 
 void			print_windows(t_cw *cw)
@@ -235,11 +205,9 @@ void			print_windows(t_cw *cw)
 	print_map(cw);
 	wrefresh(cw->visu.map);
 	print_header(cw);
-	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
-	mvwprintw(cw->visu.header, 1, 1,"%s", "** PAUSED **");
-	mvwprintw(cw->visu.header, 3, 1,"%s %d", "Cycles/second limit :", 0);
 	wrefresh(cw->visu.header);
 	print_menu(cw);
+	wrefresh(cw->visu.menu);	
 }
 
 void			init_visu(t_cw *cw)
@@ -259,10 +227,9 @@ void			init_visu(t_cw *cw)
 	init_pair(10, COLOR_WHITE, COLOR_BLACK);
 	init_pair(11, COLOR_BLACK, COLOR_GRAY);
 	init_pair(12, COLOR_BLACK, COLOR_RED);
-	getmaxyx(stdscr, cw->visu.row, cw->visu.col);
-	cw->visu.bkg = newwin(cw->visu.row, cw->visu.col, 0, 0);
-	cw->visu.map = newwin(cw->visu.row * 0.9 - 1, cw->visu.col * 3 / 4 - 1, 1, 1);
-	cw->visu.header = newwin(cw->visu.row * 0.9 - 1, cw->visu.col / 4 - 1, 1, cw->visu.col * 3 / 4 + 1);
-	cw->visu.menu = newwin(cw->visu.row * 0.1 - 1, cw->visu.col - 2, cw->visu.row * 0.9 + 1, 1);
+	cw->visu.bkg = newwin(80, 264, 0, 0);
+	cw->visu.map = newwin(67, 194, 1, 1);
+	cw->visu.header = newwin(67, 67, 1, 196);
+	cw->visu.menu = newwin(10, 262, 69, 1);
 	print_windows(cw);	
 }
--- src/vs/vs_log.c
+++ src/vs/vs_log.c
@@ -19,3 +19,34 @@ void				vs_log(t_cw *cw, size_t i_car, uint8_t i_op)
 	cw->visu.st_op = st_add(cw, i_car, cw->visu.st_op);
 	vs_backlight_map(cw, cw->visu.st_op, 1);
 }
+
+void				print_live_breakdown(t_cw *cw, int mod)
+{
+	uint			k;
+	uint			i;
+	uint			j;
+	uint			total;
+
+	k = (mod < 0) ? 11 + 4 * cw->num_of_champs : 13 + 4 * cw->num_of_champs;
+	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);
+	i = 0;
+	total = 0;
+	while (i < cw->num_of_champs)
+		total = total + cw->champ[i++].lives;
+	i = 1;
+	j = 0;
+	total = (!total) ? cw->num_of_champs : total;
+	mvwprintw(cw->visu.header, k, i, "[");	
+	while (++i < 50)
+	{
+		
+		if (i < 48 / total * (3 + cw->champ[j].lives))
+			wattron(cw->visu.header, COLOR_PAIR(j++ * 2 + 1) | A_BOLD);	
+		mvwprintw(cw->visu.header, k, i, "-");
+	}
+	wattron(cw->visu.header, COLOR_PAIR(10) | A_BOLD);	
+	mvwprintw(cw->visu.header, k, i, "]");
+	wrefresh(cw->visu.header);
+}
+
+
